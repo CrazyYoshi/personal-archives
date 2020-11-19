4a65de34b869999594cfdd8d99e0c6701a1c4b19
Miloud Maamar <development@maamar.fr>
2020-11-19 09:43:05 +0100

    tag images

--- run-personal-archives.ps1
+++ run-personal-archives.ps1
@@ -1,4 +1,4 @@
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
-docker build $PSScriptRoot  -t personal-archives:latest
-docker run -d -p 80:80 -p 81:81 -p 82:82 -p 83:83 personal-archives:latest
+docker build $PSScriptRoot  -t crazyyoshi/personal-archives:latest
+docker run -d -p 80:80 -p 81:81 -p 82:82 -p 83:83 crazyyoshi/personal-archives:latest
